ca51b23c2cc81014dc57418ddf8a3fdaf986e265
Babsmutini <scotteyawo@gmail.com>
2021-12-14 16:15:32 +0100

    Init app

---
+++ src/features/dogs/dogsApiSlice.ts
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const DOGS_API_KEY = "9c94643a-d6b2-40dc-8dfa-c7b264e514e2";
+
+interface Breed {
+  id: string;
+  name: string;
+  image: {
+    url: string;
+  };
+}
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.thedogapi.com/v1",
+    prepareHeaders(headers) {
+      headers.set("x-api-key", DOGS_API_KEY);
+      return headers;
+    },
+  }),
+  endpoints(builder) {
+    return {
+      fetchBreeds: builder.query<Breed[], number | void>({
+        query(limit = 10) {
+          return `/breeds?limit=${limit}`;
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchBreedsQuery, } = apiSlice;
